9981fb1f0415adcbce6f4537bb0a701723f946ed
Debapriya Padhy <debapriya.padhy@flipkart.com>
2017-10-24 12:47:28 +0530

    Add size based log rotation

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/FoxtrotServer.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/FoxtrotServer.java
@@ -15,6 +15,16 @@
  */
 package com.flipkart.foxtrot.server;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.filter.ThresholdFilter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.rolling.FixedWindowRollingPolicy;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy;
+import ch.qos.logback.core.spi.FilterAttachable;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -52,9 +62,13 @@ import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.config.LoggingConfiguration;
+import com.yammer.dropwizard.logging.AsyncAppender;
+import com.yammer.dropwizard.logging.LogFormatter;
 import com.yammer.metrics.core.HealthCheck;
 import net.sourceforge.cobertura.CoverageIgnore;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,6 +94,11 @@ public class FoxtrotServer extends Service<FoxtrotServerConfiguration> {
     public void run(FoxtrotServerConfiguration configuration, Environment environment) throws Exception {
         configuration.getHttpConfiguration().setRootPath("/foxtrot/*");
         configureObjectMapper(environment);
+
+        Logger root =  (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        LoggingConfiguration loggingConfiguration  = configuration.getLoggingConfiguration();
+        LoggingConfiguration.FileConfiguration file = loggingConfiguration.getFileConfiguration();
+        root.addAppender(AsyncAppender.wrap(setupLogging(file, root.getLoggerContext(), file.getLogFormat())));
 
         ObjectMapper objectMapper = environment.getObjectMapperFactory().build();
         ExecutorService executorService = environment.managedExecutorService("query-executor-%s", 20, 40, 30, TimeUnit.SECONDS);
@@ -139,6 +158,61 @@ public class FoxtrotServer extends Service<FoxtrotServerConfiguration> {
         environment.getObjectMapperFactory().disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         SubtypeResolver subtypeResolver = new StdSubtypeResolver();
         environment.getObjectMapperFactory().setSubtypeResolver(subtypeResolver);
+    }
+
+    private FileAppender<ILoggingEvent> setupLogging(LoggingConfiguration.FileConfiguration file,
+                                                     LoggerContext context,
+                                                     com.google.common.base.Optional<String> logFormat) {
+
+        final LogFormatter formatter = new LogFormatter(context, file.getTimeZone());
+
+        for (String format : logFormat.asSet()) {
+            formatter.setPattern(format);
+        }
+        formatter.start();
+
+        final FileAppender<ILoggingEvent> appender =
+                file.isArchive() ? new RollingFileAppender<>() :
+                        new FileAppender<ILoggingEvent>();
+
+        appender.setAppend(true);
+        appender.setContext(context);
+        appender.setLayout(formatter);
+        appender.setFile(file.getCurrentLogFilename());
+        appender.setPrudent(false);
+
+        addThresholdFilter(appender, file.getThreshold());
+
+        if (file.isArchive()) {
+
+            SizeBasedTriggeringPolicy triggeringPolicy = new SizeBasedTriggeringPolicy("100MB");
+
+            final FixedWindowRollingPolicy windowRollingPolicy = new FixedWindowRollingPolicy();
+            windowRollingPolicy.setMinIndex(0);
+            windowRollingPolicy.setMaxIndex(file.getArchivedFileCount());
+            String filePattern = file.getArchivedLogFilenamePattern().replaceAll("-%d\\{.*\\}","");
+            windowRollingPolicy.setFileNamePattern(filePattern);
+            windowRollingPolicy.setContext(context);
+
+            ((RollingFileAppender<ILoggingEvent>) appender).setRollingPolicy(windowRollingPolicy);
+            ((RollingFileAppender<ILoggingEvent>) appender).setTriggeringPolicy(triggeringPolicy);
+
+            windowRollingPolicy.setParent(appender);
+            windowRollingPolicy.start();
+        }
+
+        appender.stop();
+        appender.start();
+
+        return appender;
+
+    }
+
+    private static void addThresholdFilter(FilterAttachable<ILoggingEvent> appender, Level threshold) {
+        final ThresholdFilter filter = new ThresholdFilter();
+        filter.setLevel(threshold.toString());
+        filter.start();
+        appender.addFilter(filter);
     }
 
 }
